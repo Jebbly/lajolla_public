9820cea69f7e686981449352078d4a223d0af1a8
Mitchell Bizzigotti <mbizzigotti@ucsd.edu>
2026-01-06 11:15:38 -0800

    Added compiled Embree library for Apple arm64

--- cmake/FindEmbree.cmake
+++ cmake/FindEmbree.cmake
@@ -5,8 +5,14 @@ find_path(EMBREE_INCLUDE_PATH embree4/rtcore.h
   /opt/local/include)
 
 if (APPLE)
+    if ("arm64" STREQUAL CMAKE_SYSTEM_PROCESSOR)
+    set(EMBREE_LIBRARY_DIR ${CMAKE_SOURCE_DIR}/embree/lib-macos/arm64)
+    message("-- Detected Apple Silicon (using Embree for arm64)")
+    else ()
+    set(EMBREE_LIBRARY_DIR ${CMAKE_SOURCE_DIR}/embree/lib-macos)
+    endif ()
 find_library(EMBREE_LIBRARY NAMES embree4 PATHS
-  ${CMAKE_SOURCE_DIR}/embree/lib-macos
+    ${EMBREE_LIBRARY_DIR}
   /usr/lib
   /usr/local/lib
   /opt/local/lib)
